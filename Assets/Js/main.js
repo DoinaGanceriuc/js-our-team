1ed4e7e1e8b39479282ede0bf6dd4ae5da7ac556
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-22 15:14:37 +0200

    Add basic structure html and js

---
+++ Assets/Js/main.js
@@ -0,0 +1,10 @@
+/* 
+Il nostro compito é quello di rappresentare, utilizzando la struttura dati piú appropriata, una lista di membri di un team.
+Ciascun membro del team avrá come caratteristica:
+     name, 
+     role,
+     image
+- Per le immagini potete mettere il percorso di un immagine presa da internet, provate con lorem pixum (https://picsum.photos/images)
+
+- Stampare in console tutti gli elementi del team e le loro proprieta.
+*/
